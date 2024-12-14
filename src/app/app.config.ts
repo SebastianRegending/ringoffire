6566f4b734b1bfb367f0906c0b06862290f8500c
Sebastian Regending <sebastianr@web.de>
2024-12-14 21:02:43 +0100

    first playable version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,48 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, NgModule } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { AppComponent } from './app.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+import { GameComponent } from './game/game.component';
+import { PlayerComponent } from './player/player.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+@NgModule({
+  declarations: [
+    // AppComponent,
+    // StartScreenComponent,
+    // GameComponent,
+    // PlayerComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule,
+    Component
+  ],
+  providers: [],
+  // bootstrap: [AppComponent]
+})
+
+export class AppModule { }
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
 };
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,11 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Game } from '../../../src/models/game';
 import { OnInit } from '@angular/core';
+import { PlayerComponent } from '../player/player.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { GameInfoComponent } from "../game-info/game-info.component";
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent, MatIconModule, MatButtonModule, GameInfoComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
@@ -16,7 +22,7 @@ export class GameComponent implements OnInit {
   currentCard: string | undefined = '';
   game!: Game;
 
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
 
 
     ngOnInit(): void {
@@ -36,7 +42,8 @@ export class GameComponent implements OnInit {
       this.pickCardAnimation = true;
       console.log(this.currentCard);  
       console.log(this.game);
-      
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
       setTimeout(() => {
         if (this.currentCard !== undefined) {
           this.game.playedCards.push(this.currentCard);
@@ -44,6 +51,15 @@ export class GameComponent implements OnInit {
         this.pickCardAnimation = false;
       }, 1250);
     }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if(name && name.length > 0) {
+      this.game.players.push(name);
+    }
+    });
   }
 
 }
